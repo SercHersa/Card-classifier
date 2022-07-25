6b9674c7ceba3ec286ff4fbe4e05d37a5c10611c
SercHersa <i.sergio.hersa@gmail.com>
2022-07-25 16:52:34 -0500

    Control del robot, lo cual implica el control de ambos motores

---
+++ CrdC-Codes/11-Control_PID-Robot/11-Control_PID-Robot.ino
@@ -0,0 +1,214 @@
+#include "motorControl.h"
+
+#define EnML 15
+#define ML_A 2
+#define ML_B 4
+
+#define EnMR 5
+#define MR_A 16
+#define MR_B 17
+
+#define MLenc_A 21 // Amarillo
+#define MLenc_B 19 // Blanco
+
+#define MRenc_A 22 // Amarillo
+#define MRenc_B 23 // Blanco
+
+// Configurando los parámetros de PWM
+const int frecuencia = 10000;
+const int canal_L = 0;
+const int canal_R = 1;
+const int resolucion = 12;
+
+volatile long L_pulses = 0;
+
+volatile long R_pulses = 0;
+
+double wLRef = 0.0;        // Velocidad angular en rad/s
+double wL = 0.0;           // Velocidad angular de referencia en rad/s
+
+double wRRef = 0.0;        // Velocidad angular en rad/s
+double wR = 0.0;           // Velocidad angular de referencia en rad/s
+
+const double constValue = 11.18;
+//const double constValue = 0.3913;
+
+int cvL = 0;
+int cvR = 0;
+
+unsigned long lastTime = 0; // Tiempo anterior
+unsigned long sampleTime = 100; // Tiempo de muestreo
+unsigned int R = 562; // Resolucion del encoder R = mH*s*r
+
+String inputString = "";
+bool stringComplete = false;
+const char separator = ',';
+const int datoLength = 2;
+double dato[datoLength];
+
+motorControl motorL(sampleTime);
+motorControl motorR(sampleTime);
+
+
+//--------------setup------------
+void setup() {
+  pinMode(EnML, OUTPUT);
+  pinMode(ML_A, OUTPUT);
+  pinMode(ML_B, OUTPUT);
+
+  pinMode(EnMR, OUTPUT);
+  pinMode(MR_A, OUTPUT);
+  pinMode(MR_B, OUTPUT);
+  
+  pinMode(MLenc_A, INPUT);    
+  pinMode(MLenc_B, INPUT);
+
+  pinMode(MRenc_A, INPUT);    
+  pinMode(MRenc_B, INPUT);
+
+  ledcSetup(canal_L, frecuencia, resolucion);
+  ledcSetup(canal_R, frecuencia, resolucion);
+
+  ledcAttachPin(EnML, canal_L);
+  ledcAttachPin(EnMR, canal_R);
+  
+  attachInterrupt(MLenc_A, encoderL, RISING);
+  attachInterrupt(MRenc_A, encoderR, RISING);
+
+  Serial.begin(115200);
+
+  motorL.setGains(0.45, 0.04, 0.04);           //K, Ti, Td
+  motorR.setGains(0.45, 0.04, 0.04);           //K, Ti, Td
+  
+  motorL.setCvLimits(4095,1900);               // valores límites de pwm, para vel máxima y minima
+  motorL.setPvLimits(19,0);                    // valores límites en rad/s
+
+  motorR.setCvLimits(4095,1900);               // valores límites de pwm, para vel máxima y minima
+  motorR.setPvLimits(19,0);                    // valores límites en rad/s
+
+  lastTime = millis();
+}
+
+
+//--------------loop------------
+void loop() {
+  if (stringComplete) {
+    for (int i = 0; i < datoLength; i++){
+      int index = inputString.indexOf(separator);
+      dato[i] = inputString.substring(0,index).toFloat();
+      inputString = inputString.substring(index + 1);
+    }
+    wLRef = dato[0];
+    wRRef = dato[1];
+    
+    inputString = ""; 
+    stringComplete = false; 
+  }
+  
+  velValue();
+  
+}
+
+
+//--------------functions------------
+void serialEvent(){
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar; 
+    if (inChar == '\n') {
+      stringComplete = true; 
+    } 
+  }
+}
+
+void velValue(){
+  if (millis() - lastTime >= sampleTime){ // Se actualiza cada sampleTime (milisegundos)
+    
+    wL =(constValue*L_pulses)/((millis()-lastTime));// Velocidad en RPM
+    wR =(constValue*R_pulses)/((millis()-lastTime));// Velocidad en RPM
+    
+    lastTime = millis();            // Almacenamos el tiempo actual
+    
+    L_pulses = 0;                   // Reiniciamos los pulsos
+    R_pulses = 0;                   // Reiniciamos los pulsos
+    
+    cvL = motorL.compute(wLRef,wL);           // Control PID
+    cvR = motorR.compute(wRRef,wR);           // Control PID
+    
+    if (cvL > 1) clockwiseL (cvL);
+    else if (cvL < 1) anticlockwiseL(abs(cvL));
+    else anticlockwiseL (0);
+
+    if (cvR > 1) clockwiseR (cvR);
+    else if (cvR < 1) anticlockwiseR(abs(cvR));
+    else anticlockwiseR (0);
+    
+    Serial.println(wL);
+    Serial.println(wR);
+    
+  }
+}
+
+void encoderL(){
+  if (digitalRead(MLenc_B) == HIGH){     // si B es HIGH, sentido horario
+    L_pulses++ ;        // incrementa POSICION en 1
+  }
+  else {          // si B es LOW, sentido anti horario
+    L_pulses-- ;        // decrementa POSICION en 1
+  }
+
+}
+
+void encoderR(){
+  if (digitalRead(MRenc_B) == HIGH){     // si B es HIGH, sentido horario
+    R_pulses-- ;        // incrementa POSICION en 1
+  }
+  else {          // si B es LOW, sentido anti horario
+    R_pulses++ ;        // decrementa POSICION en 1
+  }
+
+}
+
+void clockwiseL(int vel){
+  digitalWrite(ML_A, LOW);
+  digitalWrite(ML_B, HIGH);
+  ledcWrite(canal_L, vel);
+  if (vel < 1) {
+    digitalWrite(ML_A, LOW);
+    digitalWrite(ML_B, LOW);
+    ledcWrite(canal_L, 0);
+  }
+}
+
+void anticlockwiseL(int vel){
+  digitalWrite(ML_A, HIGH);
+  digitalWrite(ML_B, LOW);
+  ledcWrite(canal_L, vel);
+  if (vel < 1) {
+    digitalWrite(ML_A, LOW);
+    digitalWrite(ML_B, LOW);
+    ledcWrite(canal_L, 0);
+  }
+}
+
+void clockwiseR(int vel){
+  digitalWrite(MR_A, LOW);
+  digitalWrite(MR_B, HIGH);
+  ledcWrite(canal_R, vel);
+  if (vel < 1) {
+    digitalWrite(MR_A, LOW);
+    digitalWrite(MR_B, LOW);
+    ledcWrite(canal_R, 0);
+  }
+}
+
+void anticlockwiseR(int vel){
+  digitalWrite(MR_A, HIGH);
+  digitalWrite(MR_B, LOW);
+  ledcWrite(canal_R, vel);
+  if (vel < 1) {
+    digitalWrite(MR_A, LOW);
+    digitalWrite(MR_B, LOW);
+    ledcWrite(canal_R, 0);
+  }
+}
